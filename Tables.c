28f3503edfdb756cf637f26cebfe08fc313fa6f5
THARUNKUMAR1126 <119719161+THARUNKUMAR1126@users.noreply.github.com>
2022-12-22 06:30:56 +0530

    #include<stdio.h> int main() {     int i,n,k;     scanf("%d%d",&n,&k);     for(i=1;i<=k;i++)     {         if(i%2==1)         {             printf("%d x %d = %d ",n,i,n*i);         }     } }

---
+++ Tables.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,k;
+    scanf("%d%d",&n,&k);
+    for(i=1;i<=k;i++)
+    {
+        if(i%2==1)
+        {
+            printf("%d x %d = %d
+",n,i,n*i);
+        }
+    }
+}
