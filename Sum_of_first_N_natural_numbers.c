ba03dac7dd6559aac2cd0b0ddcc46376a054c70d
THARUNKUMAR1126 <119719161+THARUNKUMAR1126@users.noreply.github.com>
2023-01-28 10:58:24 +0530

    #include<stdio.h> int main() {     int n,sum=0;     scanf("%d",&n);     sum = n*(n+1)/2;     printf("%d",sum); }

--- Sum_of_first_N_natural_numbers.c
+++ Sum_of_first_N_natural_numbers.c
@@ -1,11 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,s=0;
+    int n,sum=0;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    {
-        s=n*(n+1)/2;    
-    }
-    printf("%d",s);
+    sum = n*(n+1)/2;
+    printf("%d",sum);
 }
